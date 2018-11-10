bfe191235d5d563b5173c5291d54ffafd56daaff
Aleksandra Borczak <ola.borczak@gmail.com>
2018-11-10 16:25:41 +0100

    Updates

---
+++ precache-manifest.17704cbfb1cefd425a506d58c577b32b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec46125784e0b1f6cca6",
+    "url": "/react-app/static/js/runtime~main.ec461257.js"
+  },
+  {
+    "revision": "c2b44721e067519bb336",
+    "url": "/react-app/static/js/main.c2b44721.chunk.js"
+  },
+  {
+    "revision": "4ddfddf36b8fcf811772",
+    "url": "/react-app/static/js/1.4ddfddf3.chunk.js"
+  },
+  {
+    "revision": "c2b44721e067519bb336",
+    "url": "/react-app/static/css/main.e79fcaf9.chunk.css"
+  },
+  {
+    "revision": "eebaa13b93caac0e046ba54604d98a61",
+    "url": "/react-app/index.html"
+  }
+];
